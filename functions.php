bf474c73487b103b6b0080954a65dca0c71c549f
Tyler Giordani <tgiordani567@gmail.com>
2018-05-17 11:08:09 -0400

    Updated format and added user_group_memberships function

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+
 <?php
 
 // enqueue styles for child theme
@@ -63,39 +64,43 @@ remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_
 remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
 
 
-
 function gci_show_product_info() {
+	//$id = get_the_ID();
+	//var_dump(get_post($id));
+	global $product;
 
+		$url = $product->get_attribute( 'pa_product_link' );
+		$clickable_url = "<a target='_blank' href='".$url."'>Visit Link</a>";
 
-
-	echo "<div class='gci_wc_product'>";
-
-
-
-		echo "<div style='margin-bottom: -10%; float:left;border:1px solid red; width:20%;'>";
+		echo "<td class='gci-product-table-td' width='15%'>";
 		echo get_the_post_thumbnail();
-		echo "</div>";
+		echo "</td>";
 
-		echo "<div style='margin-bottom: 0; float:left;border:1px solid red; width:30%;'>";
+		echo "<td class='gci-product-table-td' width='15%'>";
+		echo $product->get_attribute( 'pa_Company' );
+		echo "</td>";
+
+		echo "<td class='gci-product-table-td' width='15%'>";
 		echo get_the_title();
-		echo "</div>";
+		echo "</td>";
 
-		echo "<div style='margin-bottom: 0; float:left;border:1px solid red; width:40%;'>";
+		echo "<td class='gci-product-table-td' width='50%'>";
 		echo get_the_excerpt();
-		echo "The excerpt will go here";
-		echo "</div>";
+		echo "</td>";
 
-		echo "<div style='margin-bottom: 0; float:left;border:1px solid red; width:10%;'>";
-		echo "and link here";
-		echo "</div>";
+		echo "<td class='gci-product-table-td' width='5%'>";
+		echo $clickable_url;
+		echo "</td>";
 
-	echo "</div>";
 
 
 
 }
+
 add_action('woocommerce_after_shop_loop_item_title', 'gci_show_product_info');
 
+
+// Add custom Company field to 'Edit Page' in dashboard
 add_action( 'add_meta_boxes', 'cd_meta_box_add_company' );
 function cd_meta_box_add_company() {
     add_meta_box( 'my-meta-box-id', 'Company Name:', 'cd_meta_box_cb', 'page', 'normal', 'high' );
@@ -131,56 +136,25 @@ function cd_meta_box_save_company( $post_id ) {
         update_post_meta( $post_id, 'Company', wp_kses( $_POST['Company'], $allowed ) );
 }
 
-function user_membership_field( $user ) {
-    $membership_status = get_the_author_meta( 'membership', $user->ID);
-		?>
-    <h3><?php _e('PMBus User Information'); ?></h3>
-    <table class="form-table">
-        <tr>
-            <th>
-            <label for="Membership Type"><?php _e('PMBus User Type'); ?>
-            </label></th>
-            <td><span class="description"></span><br>
-            <label><input type="radio" name="membership" <?php if ($membership_status == 'Full-Member-Admin' ) { ?>checked="checked"<?php }?> value="Full-Member-Admin">Full Member Admin<br /></label>
-            <label><input type="radio" name="membership" <?php if ($membership_status == 'Full-Member' ) { ?>checked="checked"<?php }?> value="Full-Member">Full Member<br /></label>
-						<label><input type="radio" name="membership" <?php if ($membership_status == 'Tools-Member' ) { ?>checked="checked"<?php }?> value="Tools-Member">Tools Member<br /></label>
-            </td>
-
-				</tr>
-
-				<tr>
-        		<th>
-            <label for="company_name"><?php _e('Company'); ?>
-            </label></th>
-          	<td>
-            <span class="description"><?php _e('Insert Your Company name'); ?></span><br>
-            <input type="text" name="company_name" id="company_name" value="<?php echo esc_attr( get_the_author_meta( 'company_name', $user->ID ) ); ?>" class="regular-text" /><br />
-          	</td>
-        </tr>
+//-----------Create a new widget area
+//function pmbus_company_sidebar_init() {
+		//register_sidebar(
+			//array(
+			//'name' => 'Company Events Sidebar',
+			//'id' => 'company_events_sidebar',
+			//'before_widget' => '<aside>',
+			//'after_widget' => '</aside>',
+			//'before_title' => '<h3 class="widget-title">',
+			//'after_title' => '</h3>',
+		//));
+//}
 
 
+//add_action('widgets_init', 'pmbus_company_sidebar_init');
 
-    </table>
-		<?php
-}
+//hook into the init action and call create_topics_nonhierarchical_taxonomy when it fires
 
-function my_save_custom_user_profile_fields( $user_id ) {
-    if ( !current_user_can( 'edit_user', $user_id ) )
-        return FALSE;
-
-    update_usermeta( $user_id, 'membership', $_POST['membership'] );
-		update_usermeta( $user_id, 'company_name', $_POST['company_name'] );
-
-
-}
-
-add_action( 'show_user_profile', 'user_membership_field' );
-add_action( 'edit_user_profile', 'user_membership_field' );
-add_action( 'user_new_form', 'user_membership_field' );
-add_action( 'personal_options_update', 'my_save_custom_user_profile_fields' );
-add_action( 'edit_user_profile_update', 'my_save_custom_user_profile_fields' );
-add_action( 'user_new_update', 'my_save_custom_user_profile_fields' );
-
+// Add menu item for draft posts
 add_action( 'admin_menu', 'addPMBusMenu' );
 
 function addPMBusMenu(){
@@ -208,6 +182,7 @@ function addPMBusMenu(){
 		  'add_new_members_function');
 }
 
+// display links to the groups a user is member of
 function pmbus_members_page_build() {
 	?>
 	<div style='padding-right: 6%; padding-left: 3%;'>
@@ -363,43 +338,174 @@ function pmbus_members_page_build() {
 	<?php
 	}
 
-function create_new_user()
-		{
-			$username = 'TestUser';
-			$password = 'TempPass01';
-			$email = 'TestUser@pmbus.org';
-			$user_role = 'Author';
-			$company_name = 'Test Company';
-			$membership = 'Full-Member-Admin';
-			$group = '8';
+function add_new_members_function() { ?>
+	<div style='padding-right: 6%; padding-left: 3%;'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
 
-			$user_id = wp_create_user( $username, $password, $email );
-			$user = new WP_User( $user_id );
-			$user->set_role('author');
-			wp_update_user(array(
-	        'ID' => $user_id,
-	        'membership' => $membership,
-					'company_name' => $company_name,
-	    ));
-
-			echo $username . " has been created"; ?>
-			<p>Username: <?php echo $username; ?></p>
-			<p>Password: <?php echo $password; ?></p>
-			<p>Email: <?php echo $email; ?></p>
-			<p>User Role: <?php echo $user_role; ?></p>
-			<p>Company: <?php echo $company_name; ?></p>
-			<p>Membership: <?php echo $membership; ?></p>
-
-			<?php
+	<style>
+	* {
+    	box-sizing: border-box;
 		}
+	</style>
 
-		if(array_key_exists('test',$_POST)){
-			$users = get_users( array( 'fields' => array( 'ID' ) ) );
-				foreach($users as $user_id){
-	        print_r(get_user_meta ( $user_id->ID));
-					echo "<br><br>";
-	    }
-			//create_new_user();
+
+	<body>
+	<h2 style='font-size: 2em; text-align: center;'>Add New Member</h2>
+	<form method="post">
+    <input type="submit" name="test" id="test" value="Submit" /><br/>
+	</form>
+	<?php
+
+function create_new_user()
+	{
+		$username = 'TestUser';
+		$password = 'TempPass01';
+		$email = 'TestUser@pmbus.org';
+		$user_role = 'Author';
+		$company_name = 'Test Company';
+		$membership = 'Full-Member-Admin';
+		$group = '8';
+
+		$user_id = wp_create_user( $username, $password, $email );
+		$user = new WP_User( $user_id );
+		$user->set_role('author');
+		wp_update_user(array(
+        'ID' => $user_id,
+        'membership' => $membership,
+				'company_name' => $company_name,
+    ));
+
+		echo $username . " has been created"; ?>
+		<p>Username: <?php echo $username; ?></p>
+		<p>Password: <?php echo $password; ?></p>
+		<p>Email: <?php echo $email; ?></p>
+		<p>User Role: <?php echo $user_role; ?></p>
+		<p>Company: <?php echo $company_name; ?></p>
+		<p>Membership: <?php echo $membership; ?></p>
+
+		<?php
+	}
+
+	if(array_key_exists('test',$_POST)){
+		$users = get_users( array( 'fields' => array( 'ID' ) ) );
+			foreach($users as $user_id){
+        print_r(get_user_meta ( $user_id->ID));
+				echo "<br><br>";
+    }
+		//create_new_user();
+	}
+	}
+
+
+
+add_shortcode('groups_users_list_members', 'groups_users_list_members');
+function groups_users_list_members( $atts, $content = null ) {
+	$output = "";
+	$options = shortcode_atts(
+			array(
+					'group_id' => null
+			),
+			$atts
+	);
+	if ($options['group_id']) {
+		$group = new Groups_Group($options['group_id']);
+		if ($group) {
+			$users = $group->__get("users");
+			if (count($users)>0) {
+				foreach ($users as $group_user) {
+					$user = $group_user->user;
+					$user_info = get_userdata($user->ID);
+
+					$output .= "<tr>
+    											<td width='20%'>" . $user_info-> company_name . "</td>
+    											<td width='20%'>" . $user_info-> user_login . "</td>
+													<td  class='one' width='20%'>" . $user_info-> user_email . "</td>
+													<td  class='one' width='20%'>" . $user_info-> billing_phone . "</td>
+													<td width='20%'>" . $user_info-> membership . "</td>";
+      			}
+			}
 		}
 	}
-?>
+	echo $output;
+}
+
+
+function user_membership_field( $user ) {
+    $membership_status = get_the_author_meta( 'membership', $user->ID);
+		?>
+    <h3><?php _e('PMBus User Information'); ?></h3>
+    <table class="form-table">
+        <tr>
+            <th>
+            <label for="Membership Type"><?php _e('PMBus User Type'); ?>
+            </label></th>
+            <td><span class="description"></span><br>
+            <label><input type="radio" name="membership" <?php if ($membership_status == 'Full-Member-Admin' ) { ?>checked="checked"<?php }?> value="Full-Member-Admin">Full Member Admin<br /></label>
+            <label><input type="radio" name="membership" <?php if ($membership_status == 'Full-Member' ) { ?>checked="checked"<?php }?> value="Full-Member">Full Member<br /></label>
+						<label><input type="radio" name="membership" <?php if ($membership_status == 'Tools-Member' ) { ?>checked="checked"<?php }?> value="Tools-Member">Tools Member<br /></label>
+            </td>
+
+				</tr>
+
+				<tr>
+        		<th>
+            <label for="company_name"><?php _e('Company'); ?>
+            </label></th>
+          	<td>
+            <span class="description"><?php _e('Insert Your Company name'); ?></span><br>
+            <input type="text" name="company_name" id="company_name" value="<?php echo esc_attr( get_the_author_meta( 'company_name', $user->ID ) ); ?>" class="regular-text" /><br />
+          	</td>
+        </tr>
+
+
+
+    </table>
+		<?php
+}
+
+
+function my_save_custom_user_profile_fields( $user_id ) {
+    if ( !current_user_can( 'edit_user', $user_id ) )
+        return FALSE;
+
+    update_usermeta( $user_id, 'membership', $_POST['membership'] );
+		update_usermeta( $user_id, 'company_name', $_POST['company_name'] );
+
+
+}
+
+add_action( 'show_user_profile', 'user_membership_field' );
+add_action( 'edit_user_profile', 'user_membership_field' );
+add_action( 'user_new_form', 'user_membership_field' );
+add_action( 'personal_options_update', 'my_save_custom_user_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_custom_user_profile_fields' );
+add_action( 'user_new_update', 'my_save_custom_user_profile_fields' );
+
+function user_group_memberships( $user_id = null, $show_hidden = false ) {
+
+	$group_ids = groups_get_user_groups($user_id);
+
+	$visible_group_ids = array();
+
+	foreach($group_ids["groups"] as $group_id) {
+		if (!$show_hidden) {
+			if(groups_get_group(array( 'group_id' => $group_id )) -> status !== 'hidden') {
+			$visible_group_ids[] = $group_id;
+			}
+		} else {
+		$visible_group_ids[] = $group_id;
+		}
+	}
+
+	if (empty($visible_group_ids)) {
+		echo 'None';
+	} else {
+		foreach($visible_group_ids as $visible_group_id) {
+			echo(
+				'<a title="View group page" href="' . home_url() . '/groups/' . groups_get_group(array( 'group_id' => $visible_group_id )) -> slug . '">' .
+				groups_get_group(array( 'group_id' => $visible_group_id )) -> name . '</a>' .
+				(end($visible_group_ids) == $visible_group_id ? '' : ', ' )
+			);
+		}
+	}
+}
