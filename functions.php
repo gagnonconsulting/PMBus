ac4b5352cd5dadc752df8e9efcad0bc61497ceac
Tyler Giordani <tgiordani567@gmail.com>
2018-05-17 10:50:58 -0400

    Added additional styling enqueue and reformatted the functions

--- functions.php
+++ functions.php
@@ -7,18 +7,43 @@
 function example_enqueue_styles() {
 
 	// enqueue parent styles
-	wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
+	wp_enqueue_style(
+		'parent-theme',
+		get_template_directory_uri() . '/style.css'
+	);
 
 }
-
 add_action('wp_enqueue_scripts', 'example_enqueue_styles');
 
-// enqueue divi child styles - dependencies are in array
+  // enqueue divi child styles - dependencies are in array
 function divi_child_enqueue() {
-wp_enqueue_style('divi_child_styles', get_stylesheet_directory_uri() . '/style.css', array('parent-theme', 'woocommerce-general', 'woocommerce-smallscreen', 'woocommerce-layout'), '1.0.0', 'all');
+	wp_enqueue_style(
+		'divi_child_styles',
+		get_stylesheet_directory_uri() . '/style.css',
+		array(
+			'parent-theme',
+			'woocommerce-general',
+			'woocommerce-smallscreen',
+			'woocommerce-layout'),
+			'1.0.0',
+			'all'
+		);
 }
 
 add_action('wp_enqueue_scripts', 'divi_child_enqueue');
+
+function theme_enqueue_styles() {
+    wp_enqueue_style(
+				'parent-style',
+				get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style(
+				'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 // take related products off of product listing page
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
