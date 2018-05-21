5a1c15bf66735849cad62e91fc85fc5cc2175c98
Tyler Giordani <tgiordani567@gmail.com>
2018-05-21 14:51:55 -0400

    Added custom meta fields for Companies taxonomy Function file

--- includes/company_taxonomy.php
+++ includes/company_taxonomy.php
@@ -12,13 +12,46 @@
   }
 
 // Add term page
-function pippin_taxonomy_add_new_meta_field() {
+function companies_taxonomy_add_new_meta_field() {
 	// this will add the custom meta field to the add new term page
 	?>
 	<div class="form-field">
-		<label for="term_meta[custom_term_meta]"><?php _e( 'Example meta field', 'pippin' ); ?></label>
+		<label for="term_meta[custom_term_meta]"><?php _e( 'Phone Number:', 'companies' ); ?></label>
 		<input type="text" name="term_meta[custom_term_meta]" id="term_meta[custom_term_meta]" value="">
-		<p class="description"><?php _e( 'Enter a value for this field','pippin' ); ?></p>
+		<p class="description"><?php _e( 'Enter a value for this field','companies' ); ?></p>
 	</div>
 <?php
+}
+
+function companies_taxonomy_edit_meta_field($term) {
+
+	// put the term ID into a variable
+	$t_id = $term->term_id;
+
+	// retrieve the existing value(s) for this meta field. This returns an array
+	$term_meta = get_option( "taxonomy_$t_id" ); ?>
+	<tr class="form-field">
+	<th scope="row" valign="top"><label for="term_meta[custom_term_meta]"><?php _e( 'Phone Number:', 'companies' ); ?></label></th>
+		<td>
+			<input type="text" name="term_meta[custom_term_meta]" id="term_meta[custom_term_meta]" value="<?php echo esc_attr( $term_meta['custom_term_meta'] ) ? esc_attr( $term_meta['custom_term_meta'] ) : ''; ?>">
+			<p class="description"><?php _e( 'Enter a value for this field','companies' ); ?></p>
+		</td>
+	</tr>
+<?php
+}
+
+// Save extra taxonomy fields callback function.
+function save_taxonomy_custom_meta( $term_id ) {
+	if ( isset( $_POST['term_meta'] ) ) {
+		$t_id = $term_id;
+		$term_meta = get_option( "taxonomy_$t_id" );
+		$cat_keys = array_keys( $_POST['term_meta'] );
+		foreach ( $cat_keys as $key ) {
+			if ( isset ( $_POST['term_meta'][$key] ) ) {
+				$term_meta[$key] = $_POST['term_meta'][$key];
+			}
+		}
+		// Save the option array.
+		update_option( "taxonomy_$t_id", $term_meta );
+	}
 }
