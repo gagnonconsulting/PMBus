915729d0d5b5e589b214298b819ab9abeb91fe0b
Tyler Giordani <tgiordani567@gmail.com>
2018-05-30 10:59:39 -0400

    Updated plugins - WooCommerce was outdated. Line 19 changed from if ( ! defined( 'ABSPATH' ) ) {exit;} to defined( 'ABSPATH' ) || exit;

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -13,12 +13,10 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @version     3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 get_header( 'shop' );
 
