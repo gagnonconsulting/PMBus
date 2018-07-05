53e703e1340f3e4d6a1f78effcb1c89a44ab6574
Tyler Giordani <tgiordani567@gmail.com>
2018-07-05 15:32:26 -0400

    Company taxonomy edits, user retrieval for later one click disable function [currently on ppmbus dev testing page]

--- includes/admin_menu/dev_testing/pmbus_dev_testing_page.php
+++ includes/admin_menu/dev_testing/pmbus_dev_testing_page.php
@@ -4,12 +4,39 @@ function pmbus_dev_testing_page_build() {
   $status = 'Active';
   ?>
   <div style='padding-top: 10%; padding-right: 6%; padding-left: 3%;'>
-    <div class="toggle">
-    <label>
-      <input id="checkValue" name="checkbox" type="checkbox" />
-      <span class="slider"></span>
-    </label>
-    <p><?= $status ?></p>
+    <?php
+    $args = array(
+      'meta_query' => array(
+        array(
+          'key' => 'user_company',
+          'compare' => 'EXISTS'
+        ),
+      )
+    );
+
+      $wp_user_search = new WP_User_Query( array( 'role' => '', 'fields' => 'all_with_meta') );
+      $agents = $wp_user_search->get_results();
+
+      $uarray = array();
+      $i = 0;
+      foreach ( $agents as $agent ) {
+
+        echo $uarray[$i]->Username = $agent->user_login;
+        ?><br><?php
+        $uarray[$i]->ID = $agent->ID;
+        $uarray[$i]->Company = $agent->user_company;
+        echo $uarray[$i]->Role = $agent->roles[0];
+        ?><br><?php
+        ?>
+        <button>Disable</button>
+        <br><br><?php
+        $i++;
+      }
+
+      ?>
+      <pre>
+        <?php /** print_r($uarray); */ ?>
+      </pre>
   </div>
   </div>
   <?php
