aa3662cf312555b779e50e72397b13c19fee0e80
Tyler Giordani <tgiordani567@gmail.com>
2018-07-26 13:04:30 -0400

    Updated the default page template for new pages to be set to member_home_page.php Resolved the issues where it was set every time a page was being edited Only sets the default template once, if the page template is not defined yet

--- includes/members_pages/page_template_default.php
+++ includes/members_pages/page_template_default.php
@@ -1,8 +1,13 @@
 <?php
 
-/* function set_default_page_template() {
+function set_page_template_default() {
     global $post;
-    if ( 'page' == $post->post_type && 0 != count( get_page_templates( $post ) ) && get_option( 'page_for_posts' ) != $post->ID ) {
-             $post->page_template = "member_home_page.php";
+    if ( 'page' == $post->post_type
+        && 0 != count( get_page_templates( $post ) )
+        && get_option( 'page_for_posts' ) != $post->ID // Not the page for listing posts
+        && '' == $post->page_template // Only when page_template is not set
+    ) {
+        $post->page_template = "member_home_page.php";
     }
-} */
+}
+add_action('add_meta_boxes', 'set_page_template_default', 1);
--- includes/members_pages/set_parent_default.php
+++ includes/members_pages/set_parent_default.php
@@ -4,7 +4,10 @@ add_filter( 'wp_insert_post_data', 'wpse_59007_set_default_page_parent' );
 
 function wpse_59007_set_default_page_parent( $data )
 {
-    if ( $data['post_status'] == 'auto-draft' && $data['post_type'] == 'page' )
+    if ( $data['post_status'] == 'auto-draft' && $data['post_type'] == 'page' ){
         $data['post_parent'] = 206;
+        global $post;
+        $post->page_template = "member_home_page.php";
+    }
     return $data;
 }
